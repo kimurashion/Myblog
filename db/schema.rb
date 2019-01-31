7e8ee78071a121bf63402e71e1c9da188dbabbaa
shion <shionkimura@gmail.com>
2019-01-31 12:47:18 +0900

    local環境で動くようにした

--- db/schema.rb
+++ db/schema.rb
@@ -48,11 +48,15 @@ ActiveRecord::Schema.define(version: 2018_12_20_015218) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
     t.string "username"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
